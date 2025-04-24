a9d7edd8cefaf5782f8a5a3bb01a952e7d88cb53
Md Aidul Islam <35846452+aidul23@users.noreply.github.com>
2025-04-24 21:31:28 +0300

    Update api.js

--- client/src/helpers/api.js
+++ client/src/helpers/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8000/api/v1", // Your base URL
+  // baseURL: "http://localhost:8000/api/v1", // Your base URL
+  baseURL: "https://tampere-titans-backend.onrender.com"
 });
 
 export default api;
