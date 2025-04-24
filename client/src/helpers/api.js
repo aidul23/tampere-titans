67bb1404a450183a266522bcd486b962f3d3f900
Md Aidul Islam <aidulislam0@gmail.com>
2025-04-24 22:35:51 +0300

    base url updated

--- client/src/helpers/api.js
+++ client/src/helpers/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const api = axios.create({
   // baseURL: "http://localhost:8000/api/v1", // Your base URL
-  baseURL: "https://tampere-titans-backend.onrender.com"
+  baseURL: "https://tampere-titans-backend.onrender.com/api/v1"
 });
 
 export default api;
