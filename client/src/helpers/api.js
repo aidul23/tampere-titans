54cbf2229fb32b83345d3d44ec2de1ee5bd7b553
Md Aidul Islam <aidulislam0@gmail.com>
2025-08-23 00:05:44 +0300

    Fin Bangla

--- client/src/helpers/api.js
+++ client/src/helpers/api.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8000/api/v1", // Your base URL
-  // baseURL: "https://tampere-titans-backend.onrender.com/api/v1"
+  //baseURL: "http://localhost:8000/api/v1", // Your base URL
+  baseURL: "https://tampere-titans-backend.onrender.com/api/v1"
 });
 
 export default api;
